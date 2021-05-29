876e05303f973e4d30981c31484bf663f740610c
pytorchic <dkclasson@gmail.com>
2021-05-28 17:21:37 -0700

    code for figure 8

---
+++ run_figure_8.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+[ ! -d outputs ] && mkdir outputs
+
+TIME=300
+DISTANCE=200
+VERBOSE=0
+
+#write to a single file, rewritten every run
+filename="outputs/figure_8_results.txt"
+if [ -f $filename ]; then
+  rm "$filename"
+fi
+touch "$filename"
+
+for PACKETSZ in 64 500 1500; do
+	for CHAINLEN in 4 5 6 7 8 9 10; do
+	    # Run the NS-3 Simulation
+	    ./waf --run "scratch/figure-8.cc --simTime=$TIME --distance=$DISTANCE --packetSize=$PACKETSZ --chainLen=$CHAINLEN"
+	done
+done
+
+echo "Simulations are done!"
+
+python3 scratch/plot-figure-8.py
+
+echo "Plots are done!"
---
+++ scratch/plot-figure-8.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+import matplotlib.pyplot as plt
+
+sim_time = 0
+per_packet_sz = {}
+
+FILENAME = 'outputs/figure_8_results.txt'
+
+with open(FILENAME,'r') as f:
+    for line in f:
+        data = line.split()
+        packet_sz = int(data[0])
+        chain_len = int(data[1])
+        sim_time = float(data[2])
+        throughput = float(data[3])
+
+        # unused: next chain_len values of data are raw throughputs for each chain
+
+        per_chain_len = {}
+        if packet_sz in per_packet_sz:
+            per_chain_len = per_packet_sz[packet_sz]
+        
+        per_chain_len[chain_len * chain_len] = throughput
+        per_packet_sz[packet_sz] = per_chain_len
+
+filename = 'outputs/figure_8_plots.png'
+
+plt.figure()
+c = 1 #color value
+for packet_sz in per_packet_sz:
+    per_chain_len = per_packet_sz[packet_sz]
+
+    chain_lens = list(per_chain_len.keys())
+    throughputs = list(per_chain_len.values())
+    plot_name = "" + str(packet_sz) + " byte packets"
+    plt.plot(chain_lens, throughputs, c="C" + str(c), label=plot_name, marker='+')
+
+    print("plotting for ", str(packet_sz))
+
+    c += 1
+
+plt.title('Recreation of Figure 8 for ' + str(sim_time) + 's')
+plt.ylabel('Chain Throughput (Mbps)')
+plt.xlabel('Lattice Size (Number of Nodes)')
+plt.grid()
+plt.legend()
+plt.savefig(filename)
+print('Saving ' + filename)
